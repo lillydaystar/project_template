8da67a40879e75a7bfa7e800f01e802a577a808d
Liliia Parashchak <liliia.parashchak@gmail.com>
2024-03-20 17:26:59 +0200

    Added empty functions for io operations

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+def input_console():
+    pass
+
+
+def read_file_python(filepath):
+    pass
+
+
+def read_file_pandas(filepath):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,10 @@
+def output_console(text):
+    pass
+
+
+def write_file_python(filepath, text):
+    pass
+
+
+def write_file_pandas(filepath, text):
+    pass
